3d389f649a9c8abddfa6f8286080b62ce6f2eff8
sora-M31 <soram31@gmail.com>
2011-07-13 12:03:19 +0100

    Add functions to allocate and update id

--- include/colony.h
+++ include/colony.h
@@ -1,11 +1,14 @@
 #ifndef __COLONY_H__
 #define __COLONY_H__
 #include <vector>
+#include <list>
 
 #include "ant.h"
 #include "trail.h"
+
 namespace QtGLWindow
 {
+typedef std::list<Ant*> Antlist;
 class Colony
 {
     public:
@@ -15,10 +18,9 @@ class Colony
         void Emit(uint32_t _interval);
         void Update(const SceneObject& _home, const SceneObject& _food);
         void ApplyRule();
-
-        void UpdateTrail();
-
+        void UpdateTrail(); 
         void Move();
+        void UpdateID();
 
         /// number of ant in a colony
         uint32_t m_num;
@@ -28,6 +30,11 @@ class Colony
         /// trail create by ants in the list
         Trail m_trail;
         uint32_t m_counter;
+        std::vector<Antlist> Cell;
+        float const cellSize;
+        float const worldSize;
+    private:
+        inline uint32_t GetCellCount();
 };//end of class
 }//end of namespace
 #endif//
--- src/colony.cpp
+++ src/colony.cpp
@@ -4,14 +4,53 @@ namespace QtGLWindow
 {
 //----------------------------------------------------------------------------------------------------------------------
 Colony::Colony()
+    :cellSize(12.0),worldSize(60.0)
 {
     m_num =50;
     m_activeNum = 1;
     m_counter =0;
+
     for(uint32_t i=0;i<m_num; ++i)
     {
         m_antList.push_back(new Ant());
     }
+
+    uint32_t cellNum = GetCellCount();
+    std::cout<<cellNum;
+    for(uint32_t i =0; i< cellNum; ++i)
+    {
+        Cell.push_back(Antlist());
+    }
+#if 1
+    Vector _pos(0,0,0);
+    for(uint32_t i=0; i<m_num; ++i)
+    {
+        uint32_t ID = Hash(_pos, 12.0,60.0);
+        //should write a function to initialize ant at different initial position like Ant(_pos);
+        Cell[ID].push_back( new Ant() );
+    }
+
+    #endif
+}
+//----------------------------------------------------------------------------------------------------------------------
+void Colony::UpdateID()
+{
+#if 1
+    uint32_t cellNum = GetCellCount();
+    for( uint32_t id =0; id< cellNum; id++)
+    {
+        std::list<Ant*>::iterator it;
+        for(it =Cell[id].begin(); it!=Cell[id].end();++it)
+        {
+            uint32_t newId =  Hash( (*it)->m_pos, 12.0, 60.0 );
+            if (newId != id)
+            {
+                Cell[newId].push_back(*it);
+                Cell[id].erase(it);
+            }
+        }
+    }
+#endif
 }
 //----------------------------------------------------------------------------------------------------------------------
 Colony::~Colony()
@@ -64,5 +103,10 @@ void Colony::Update(const SceneObject& _home, const SceneObject& _food)
         m_antList[i]->Update(50,m_trail,m_antList,_home, _food);
     }
     UpdateTrail();
+}
+inline uint32_t Colony::GetCellCount()
+{
+    uint32_t count= (uint32_t)worldSize/cellSize;
+    return count * count;
 }
 }//end of namespace
