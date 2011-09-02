3a8e13a94f9cb39792850eed0202e357d2bbcea1
sora-M31 <soram31@gmail.com>
2011-09-02 17:02:22 +0100

    Deleted some debugging info

--- src/QtextPushButton.cpp
+++ src/QtextPushButton.cpp
@@ -3,6 +3,7 @@
 
 namespace Ui
 {
+//------------------------------------------------------------------------------
 QtextPushButton::QtextPushButton( const QString& text,
                                   QWidget* parent )
                                 : QPushButton(text, parent)
@@ -10,6 +11,7 @@ QtextPushButton::QtextPushButton( const QString& text,
     m_checked = false;
     connect(this,SIGNAL(released()), this, SLOT(changeText()) );
 }
+//------------------------------------------------------------------------------
 QtextPushButton::QtextPushButton(QWidget* parent )
                                 : QPushButton(parent)
 {
@@ -17,14 +19,15 @@ QtextPushButton::QtextPushButton(QWidget* parent )
     QPushButton::setText("start");
     connect(this,SIGNAL(clicked()), this, SLOT(changeText()) );
 }
+//------------------------------------------------------------------------------
 QtextPushButton::~QtextPushButton()
 {
 }
+//------------------------------------------------------------------------------
 void QtextPushButton::changeText()
 {
     if( m_checked == false)
     {
-        std::cout<<"calling \n";
         emit start();
         setText("stop");
         m_checked = true;
@@ -33,9 +36,7 @@ void QtextPushButton::changeText()
     {
         emit stop();
         setText("restart");
-        std::cout<<true<<m_checked<<"\n";
         m_checked = false;
-        std::cout<<m_checked<<"\n";
     }
 }
 }//end of namespace
